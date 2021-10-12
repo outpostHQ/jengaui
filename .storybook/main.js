8a72e970b063e6ee8b328e67744866c185822280
Vishal Kanojia <vishal.deep@live.com>
2021-10-12 15:31:27 +0530

    Storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "stories": [
+  core: { builder: "storybook-builder-vite" },
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
     "../packages/**/*.stories.mdx",
     "../packages/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]
