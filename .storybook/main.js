0365bd466a25150e072d44e21c36a777395ac100
shubham-kaushal <nsk9195@gmail.com>
2022-07-09 17:36:59 +0530

    fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,20 @@
 module.exports = {
-  core: { builder: '@storybook/builder-vite' },
   stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/*.stories.mdx',
+    '../src/*.stories.@(js|jsx|ts|tsx)',
     '../packages/**/*.stories.mdx',
     '../packages/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
 };
