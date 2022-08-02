9b6afdef6b6cdb45b1d232a7da90427d3ad958a7
shubham-kaushal <nsk9195@gmail.com>
2022-08-02 23:30:57 +0530

    upgrade tasty to 0.7.7

--- packages/new-notifications/test/notification.test.tsx
+++ packages/new-notifications/test/notification.test.tsx
@@ -2,7 +2,7 @@ import { render, getByTestId, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { wait } from '@jenga-ui/core/test';
 import { Timer } from '@jenga-ui/core';
-import { NotificationView } from '../src/NotificationView/NotificationView';
+import { NotificationView } from '../src/NotificationView';
 
 describe('<Notification />', () => {
   it('should stop timer on hover', async () => {
