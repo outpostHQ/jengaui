0caa6dd92939efcd29bad2f8aa7dcf866f7d0cf1
shubham-kaushal <nsk9195@gmail.com>
2022-09-03 15:08:53 +0530

    fix export in @jenga-ui/react

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,46 +1,164 @@
-export * from '@jenga-ui/accordion';
-export * from '@jenga-ui/active-zone';
-export * from '@jenga-ui/alert';
-export * from '@jenga-ui/alert-dialog';
-export * from '@jenga-ui/avatar';
-export * from '@jenga-ui/badge';
-export * from '@jenga-ui/button';
-export * from '@jenga-ui/button-group';
-export * from '@jenga-ui/card';
-export * from '@jenga-ui/checkbox';
-export * from '@jenga-ui/combo-box';
-export * from '@jenga-ui/content';
+import {
+    JengaTextProps,
+    Text,
+    JengaTitleProps,
+    Title,
+    JengaParagraphProps,
+    Paragraph,
+} from '@jenga-ui/content';
+
+export { Item, Section } from '@react-stately/collections';
+
+// generic components
+export { Base, Block } from '@jenga-ui/core';
+export type { JengaBlockProps } from '@jenga-ui/core';
+export { ActiveZone } from '@jenga-ui/active-zone';
+export type { JengaActiveZoneProps } from '@jenga-ui/active-zone';
 export * from '@jenga-ui/copy-snippet';
-export * from '@jenga-ui/core';
-export * from '@jenga-ui/dialog';
-export * from '@jenga-ui/file-input';
+export {
+    Grid,
+    Flex,
+    Space,
+    Flow,
+    Prefix,
+    Suffix,
+} from '@jenga-ui/layout';
+export type {
+    JengaGridProps,
+    JengaFlexProps,
+    JengaSpaceProps,
+    JengaFlowProps,
+    JengaPrefixProps,
+    JengaSuffixProps,
+} from '@jenga-ui/layout';
+export { Link } from '@jenga-ui/link';
+export { Root } from '@jenga-ui/root';
+export type { JengaRootProps } from '@jenga-ui/root';
+export { PrismCode } from '@jenga-ui/prism-code';
+export type { JengaPrismCodeProps } from '@jenga-ui/prism-code';
+export {
+    Divider,
+    Content,
+    Header,
+    Footer,
+} from '@jenga-ui/content';
+export type {
+    JengaDividerProps,
+    JengaContentProps,
+    JengaHeaderProps,
+    JengaFooterProps,
+} from '@jenga-ui/content';
+export { GridProvider, useContextStyles, StyleProvider, Provider } from '@jenga-ui/providers';
+export type { JengaGridProviderProps, useProviderProps } from '@jenga-ui/providers';
+export { Result } from '@jenga-ui/result';
+export type {
+  JengaResultProps,
+  JengaResultStatus,
+} from '@jenga-ui/result';
+export { FieldWrapper } from '@jenga-ui/form';
+export type { JengaFieldWrapperProps } from '@jenga-ui/form';
+
+// atoms
+export { Card } from '@jenga-ui/card';
+export type { JengaCardProps } from '@jenga-ui/card';
+export * from '@jenga-ui/button';
+export { Placeholder } from '@jenga-ui/placeholder';
+export type { JengaPlaceholderProps } from '@jenga-ui/placeholder';
+export { Skeleton } from '@jenga-ui/skeleton';
+export type { JengaSkeletonProps } from '@jenga-ui/skeleton';
+export { Badge } from '@jenga-ui/badge';
+export type { JengaBadgeProps } from '@jenga-ui/badge';
+export { Tag } from '@jenga-ui/tag';
+export type { JengaTagProps } from '@jenga-ui/tag';
+export { SearchInput } from '@jenga-ui/search-input';
+export type { JengaSearchInputProps } from '@jenga-ui/search-input';
+export { Submit } from '@jenga-ui/button';
+export type { JengaTextInputBaseProps } from '@jenga-ui/text-input';
+export type { JengaTextInputBaseProps as JengaTextInputProps } from '@jenga-ui/text-input';
+export { TextInput } from '@jenga-ui/text-input';
+export { TextArea } from '@jenga-ui/textarea';
+export type { JengaTextAreaProps } from '@jenga-ui/textarea';
+export { FileInput } from '@jenga-ui/file-input';
+export type { JengaFileInputProps } from '@jenga-ui/file-input';
+export { PasswordInput } from '@jenga-ui/password-input';
+export { Checkbox, CheckboxGroup } from '@jenga-ui/checkbox';
+export type { JengaCheckboxProps, JengaCheckboxGroupProps } from '@jenga-ui/checkbox';
+export { Switch } from '@jenga-ui/switch';
+export type { JengaSwitchProps } from '@jenga-ui/switch';
+export { Radio } from '@jenga-ui/radio';
+export type { JengaRadioProps, JengaRadioGroupProps } from '@jenga-ui/radio';
+export { ComboBox } from '@jenga-ui/combo-box';
+export type { JengaComboBoxProps } from '@jenga-ui/combo-box';
+export { Menu, MenuTrigger } from '@jenga-ui/menu';
+export type { JengaMenuProps, JengaMenuTriggerProps } from '@jenga-ui/menu';
+export { Select, ListBoxPopup } from '@jenga-ui/select';
+export type {
+  JengaSelectProps,
+  JengaSelectBaseProps,
+} from '@jenga-ui/select';
+export { NumberInput } from '@jenga-ui/number-input';
+export type { JengaNumberInputProps } from '@jenga-ui/number-input';
+export { Avatar } from '@jenga-ui/avatar';
+export type { JengaAvatarProps } from '@jenga-ui/avatar';
+export {
+  Dialog,
+  DialogTrigger,
+  DialogContainer,
+  DialogForm,
+} from '@jenga-ui/dialog';
+export type {
+  JengaDialogTriggerProps,
+  JengaDialogContainerProps,
+  JengaDialogProps,
+  JengaDialogFormRef,
+  JengaDialogFormProps,
+} from '@jenga-ui/dialog';
+export { Tooltip,
+    TooltipTrigger,
+    TooltipProvider,
+} from '@jenga-ui/tooltip';
+export type {
+    JengaTooltipProps,
+    JengaTooltipTriggerProps,
+    JengaTooltipProviderProps,
+} from '@jenga-ui/tooltip';
+
+export * from '@jenga-ui/alert';
+
+// molecules
+export { LegacyTabs } from '@jenga-ui/tabs';
+export type { JengaTabsProps } from '@jenga-ui/tabs';
+export { Modal } from '@jenga-ui/modal';
+export type { JengaModalProps } from '@jenga-ui/modal';
+export {
+  AlertDialog,
+  useAlertDialogAPI,
+} from '@jenga-ui/alert-dialog';
+export type { JengaAlertDialogProps } from '@jenga-ui/alert-dialog';
+
+// services
+export { notification } from '@jenga-ui/services';
+export type { JengaNotificationOptions } from '@jenga-ui/services';
+
+export const Typography = {
+  Text,
+  Title,
+  Paragraph,
+};
+
+export { Text, Title, Paragraph };
+export type { JengaTextProps, JengaTitleProps, JengaParagraphProps };
+
+export { Portal } from '@jenga-ui/portal';
+export type { PortalProps } from '@jenga-ui/portal';
 export * from '@jenga-ui/form';
-export * from '@jenga-ui/hooks';
-export * from '@jenga-ui/input';
-export * from '@jenga-ui/layout';
-export * from '@jenga-ui/link';
-export * from '@jenga-ui/menu';
-export * from '@jenga-ui/modal';
-export * from '@jenga-ui/new-notifications';
-export * from '@jenga-ui/number-input';
-export * from '@jenga-ui/overlays';
-export * from '@jenga-ui/password-input';
-export * from '@jenga-ui/placeholder';
-export * from '@jenga-ui/portal';
-export * from '@jenga-ui/prism-code';
-export * from '@jenga-ui/providers';
-export * from '@jenga-ui/radio';
-export * from '@jenga-ui/result';
-export * from '@jenga-ui/root';
-export * from '@jenga-ui/search-input';
-export * from '@jenga-ui/select';
-export * from '@jenga-ui/services';
-export * from '@jenga-ui/skeleton';
-export * from '@jenga-ui/switch';
-export * from '@jenga-ui/tabs';
-export * from '@jenga-ui/tag';
-export * from '@jenga-ui/text-input';
-export * from '@jenga-ui/toast';
-export * from '@jenga-ui/textarea';
-export * from '@jenga-ui/tooltip';
+
+export { ModalProvider } from '@react-aria/overlays';
 export * from '@jenga-ui/utils';
+export { default as copy } from 'clipboard-copy';
+export * from '@react-aria/ssr';
+export * from '@jenga-ui/new-notifications';
+export * from '@jenga-ui/toast';
+export * from '@jenga-ui/slider';
+export * from '@jenga-ui/table';
+export * from '@jenga-ui/breadcrumbs'
