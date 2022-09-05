26165215110dc9939720a82cfdd23774875d3d72
shubham-kaushal <nsk9195@gmail.com>
2022-09-05 16:51:25 +0530

    add pull_request ci

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 
-import { Avatar, Block } from '@jenga-ui/react';
+import { Avatar, Block, Root } from '@jenga-ui/react';
 
 
 import styles from '../styles/Home.module.css';
@@ -8,9 +8,11 @@ import styles from '../styles/Home.module.css';
 const Home: NextPage = () => {
   return (
     <div>
+      <Root>
       <h1> hello</h1>
       <Block> and it is this</Block>
       <Avatar icon={null}>OP</Avatar>
+      </Root>
     </div>
   );
 };
