db3680481008b30cf295e07a22d885c693df6bbe
ajeya <ajeyabhat.off@gmail.com>
2022-09-18 17:13:57 +0530

    update

--- packages/accordion/src/types.ts
+++ packages/accordion/src/types.ts
@@ -1,6 +1,7 @@
 import { PropsWithChildren, ReactElement, ReactNode, ReactText } from 'react';
+import { BaseProps, BasePropsWithoutChildren } from 'tastycss';
 
-export type AccordionProps = {
+export type AccordionProps = BaseProps & {
   children:
     | ReactElement<AccordionItemProps>
     | ReactElement<AccordionItemProps>[];
@@ -8,9 +9,14 @@ export type AccordionProps = {
 };
 export type AccordionContextType = { isLazy?: boolean };
 export type AccordionProviderProps = PropsWithChildren<{ isLazy?: boolean }>;
-export type AccordionItemProps = {
+export type AccordionItemProps = BaseProps & {
   title: ReactText;
   children: ReactNode | (() => ReactNode);
   isDefaultExpanded?: boolean;
   extra?: ReactNode;
+  isIconVisible?: boolean;
+  titleWrapperProps?: BasePropsWithoutChildren;
+  itemTitleProps?: BasePropsWithoutChildren;
+  contentWrapperProps?: BasePropsWithoutChildren;
+  disclosureIcon?: ReactElement;
 };
