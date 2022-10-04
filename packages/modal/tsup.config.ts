d247aa38857e88d247e6bde3f82bd1f14e0711cc
taranbeer <staranbeer@gmail.com>
2022-10-04 23:59:34 +0530

    removed tsup-configs

--- packages/modal/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  treeshake: true,
-  format: ['cjs', 'esm'],
-  outExtension({ format }) {
-    return { js: `.${format}.js` };
-  },
-});
