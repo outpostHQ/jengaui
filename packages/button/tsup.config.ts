1d775e304cc7a56c738eb82dd2a92f767b485b14
Mohit Bhansali <mohit@nanovest.io>
2022-10-15 15:31:24 +0800

    Removed DTS in button to test package size

--- packages/button/tsup.config.ts
+++ packages/button/tsup.config.ts
@@ -3,10 +3,11 @@ import { findUpSync } from 'find-up';
 
 export default defineConfig({
   entry: ['./src/index.ts'],
-  dts: true,
+  dts: false,
   clean: true,
   format: ['cjs', 'esm'],
   inject: process.env.JSX ? [findUpSync('react-shim.js')!] : undefined,
   external: ['react'],
+  minify: true,
   treeshake: true,
 });
