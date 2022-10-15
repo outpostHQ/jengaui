2768449ab30c6c7bcbfe074d57b9cf5a0dfc8018
shubham-kaushal <nsk9195@gmail.com>
2022-10-15 23:09:34 +0530

    fix: tsup.config

--- packages/select/tsup.config.ts
+++ packages/select/tsup.config.ts
@@ -3,9 +3,6 @@ import { findUpSync } from 'find-up';
 
 export default defineConfig({
   clean: true,
-  format: ["cjs", "esm"],                                                 
-  outExtension(ctx) {
-    return { js: `.${ctx.format}.js` }
-  },
+  format: ["cjs", "esm"],                                               
   inject: process.env.JSX ? [findUpSync('react-shim.js')!] : undefined,
 })
