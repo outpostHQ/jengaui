3d2ad9cf7eddbf7033d6ba17f1e9c2fb97b94c95
sahilshahane <sahilpshahane123@gmail.com>
2023-03-02 17:34:50 +0530

    fix: treeshaking support

--- packages/number-input/tsup.config.ts
+++ packages/number-input/tsup.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'tsup';
 import { findUpSync } from 'find-up';
+import packageJSON from './package.json';
 
 export default defineConfig({
   clean: true,
-  format: ["cjs", "esm"],                                               
+  format: ['cjs', 'esm'],
   inject: process.env.JSX ? [findUpSync('react-shim.js')!] : undefined,
-})
+  treeshake: true,
+  minify: true,
+  dts: true,
+  external: [
+    ...Object.keys(packageJSON['dependencies']),
+    ...Object.keys(packageJSON['devDependencies']),
+    ...Object.keys(packageJSON['peerDependencies']),
+  ],
+});
