50ec0279842c2912480fe1a9885fe94d516d2f41
shubham-kaushal <nsk9195@gmail.com>
2023-03-04 00:06:38 +0530

    fix: SearchInput

---
+++ packages/button/src/utils/mapProps.ts
@@ -0,0 +1,27 @@
+import { AriaButtonProps } from '@react-types/button';
+
+import { JengaButtonProps } from '../Button';
+
+/** Converts AriaButtonProps to JengaButtonProps */
+export function ariaToJengaButtonProps(
+  props: AriaButtonProps<'button'>,
+): JengaButtonProps {
+  const { type, ...filteredProps } = props;
+
+  return {
+    ...filteredProps,
+    htmlType: type,
+  };
+}
+
+/** Converts JengaButtonProps to AriaButtonProps */
+export function jengaToAriaButtonProps(
+  props: JengaButtonProps,
+): AriaButtonProps<'button'> {
+  const { htmlType, ...filteredProps } = props;
+
+  return {
+    ...filteredProps,
+    type: htmlType,
+  };
+}
